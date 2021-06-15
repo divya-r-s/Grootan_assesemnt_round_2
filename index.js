9314ff6f44aece054abd0de310bc5bd297ff96fc
divya-r-s <81807000+divya-r-s@users.noreply.github.com>
2021-06-15 11:33:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -33,3 +33,37 @@ server.listen(3000, function () {
 	console.log("Server listening on port: 3000");
 });
 
+
+ let MongoClient = require('mongodb').MongoClient;
+ let url = "mongodb://localhost:27017/";
+
+ const csvFilePath='50000 Records.csv'
+ const csv=require('csvtojson')
+
+ csv()
+ .fromFile(csvFilePath)
+ .then((jsonObj)=>{
+    console.log(jsonObj);
+
+ 		[ 
+ 		  { _id: '1', name: 'Jack Smith', address: 'Massachusetts', age: '23' },
+ 		  { _id: '2', name: 'Adam Johnson', address: 'New York', age: '27' },
+ 		  { _id: '3', name: 'Katherin Carter', address: 'Washington DC', age: '26' },
+ 		  { _id: '4', name: 'Jack London', address: 'Nevada', age: '33' },
+ 		  { _id: '5', name: 'Jason Bourne', address: 'California', age: '36' } 
+ 		]
+
+
+ 	MongoClient.connect(url, { useNewUrlParser: true ,useUnifiedTopology: true }, (err, db) => {
+ 	  if (err) throw err;
+ 	  var dbo = db.db("data");
+ 	  dbo.collection("emp2").insertMany(jsonObj, (err, res) => {
+ 		if (err) throw err;
+ 		console.log("Number of documents inserted: " + res.insertedCount);
+
+ 		db.close();
+ 	  });
+ 	});
+ 	
+ })
+
